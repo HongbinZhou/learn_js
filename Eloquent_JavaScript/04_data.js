0aa78293259a5a15123e368eff756e3bc8d4bef4
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-05-24 18:09:41 +0800

    done reverse array which is pure or sideeffect.

--- Eloquent_JavaScript/04_data.js
+++ Eloquent_JavaScript/04_data.js
@@ -34,4 +34,45 @@ range(5, 2, -1);
 range(5, 2, -2);
 range(5, 5, 10);
 
+// reverse
+function reverse_pure(list){
+    // copy the list first
+    var new_list = [];
+    for (i=0; i<list.length; i++){
+	new_list.push(list[i]);
+    }
 
+    // then reverse it by modifing it.
+    return reverse_sideeffect(new_list);
+}
+
+function reverse_sideeffect(list){
+    for (i = 0, j = list.length-1; i<j; i++, j--){
+	var tmp = list[i];
+	// console.log("i=%d, j=%d, tmp=%d", i, j, tmp)
+	list[i] = list[j];
+	list[j] = tmp;
+    }
+    return list;
+}
+
+function test(list){
+    console.log("before reverse_sideeffect, list is: ", list)
+    reverse_sideeffect(list)
+    console.log("after reverse_sideeffect, list is: ", list)
+
+    console.log("before reverse_pure, list is: ", list)
+    var new_list = reverse_pure(list)
+    console.log("after reverse_pure, list is: ", list)
+    console.log("after reverse_pure, new list is: ", new_list)
+
+    console.log("test done!")
+
+}
+console.log("--------------------------------------------------")
+console.log(">>> test1...")
+list = [1,2,3]
+test(list)
+console.log(">>> test2...")
+list = [1,2,3,4]
+test(list)
