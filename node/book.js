63f22417dfec2042ac9f240ff847dfc4f2306918
Hongbin Zhou <hongbinzhou@hotmail.com>
2015-12-22 11:39:54 +0800

    learn multi instance.

--- node/book.js
+++ node/book.js
@@ -1,5 +1,22 @@
-exports.name = "THE BOOK";
-exports.read = function ()
-{
-    console.log("I am reading: " + exports.name);
+
+// var bname = "";
+// exports.setName = function(name){
+//     bname = name;
+//     console.log("name is: ", bname)
+// }
+// exports.getName = function (){
+//     console.log("name is: ", bname)
+//     return bname;
+// }
+
+module.exports = function(){
+    var bname = "";
+    return {
+	setName: function(name){
+	    bname = name;
+	},
+	getName: function(){
+	    return bname;
+	},
+    }
 }
